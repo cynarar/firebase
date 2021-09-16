e5fc9e6b28202f3a23f1f5e12ceedf371429a83a
Cynara Peixoto <cynara.peixoto@gmail.com>
2021-09-16 16:03:19 -0300

    Projeto inicial

---
+++ agenda/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA7Jaj_Pbf6_zw7ubAJaVm2mMaFWYBwURw",
+    authDomain: "agenda-bdd2a.firebaseapp.com",
+    projectId: "agenda-bdd2a",
+    storageBucket: "agenda-bdd2a.appspot.com",
+    messagingSenderId: "587004562175",
+    appId: "1:587004562175:web:08b132699cd1e0200f1aca",
+    measurementId: "G-S1MGYHYG1C"
+  }
+};
